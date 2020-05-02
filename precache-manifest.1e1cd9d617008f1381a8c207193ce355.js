ef91a56eea056c619e5b0fbda9c5fb7057f68d7f
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2020-05-02 12:19:45 +0000

    Updates

--- precache-manifest.1e1cd9d617008f1381a8c207193ce355.js
+++ precache-manifest.1e1cd9d617008f1381a8c207193ce355.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1c9939ac0a71792613cb10c9811d6ced",
+    "revision": "9ea4e037ecd59b4014d335ee8d4b5d13",
     "url": "/index.html"
   },
   {
-    "revision": "1f36636b6f90998a7d42",
+    "revision": "e55399268361fe395975",
     "url": "/static/css/main.3566d46e.chunk.css"
   },
   {
-    "revision": "91f54b43cc4185a505ca",
-    "url": "/static/js/2.bba74c92.chunk.js"
+    "revision": "f00964375ae073e254cc",
+    "url": "/static/js/2.d3edc375.chunk.js"
   },
   {
     "revision": "72befde6bf2596e50bf761397424f051",
-    "url": "/static/js/2.bba74c92.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.d3edc375.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1f36636b6f90998a7d42",
-    "url": "/static/js/main.766ee396.chunk.js"
+    "revision": "e55399268361fe395975",
+    "url": "/static/js/main.8c35bd91.chunk.js"
   },
   {
     "revision": "f7c2ab26754a10b02d0e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b54b2940d862ac9abf991ae04fca7dc.js"
+  "/precache-manifest.1e1cd9d617008f1381a8c207193ce355.js"
 );
 
 self.addEventListener('message', (event) => {
